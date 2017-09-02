5d1e20e020a123bb3d3e43320f67b2f2f008b7d9
Jacob Gunther <jacobgunther12@gmail.com>
2017-09-01 19:11:50 -0500

    Fixed another bug

--- events/message-reaction-add.js
+++ events/message-reaction-add.js
@@ -108,7 +108,7 @@ module.exports = (bot, r) => {
                                         description: "Failed to get user information across shards."
                                     }
                                 });
-                                console.error("Failed to get user information across shards.", e);
+                                console.error("Failed to get user information across shards.", error);
                             });
                         });
                     } catch (e) {
