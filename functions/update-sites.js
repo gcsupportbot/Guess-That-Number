8db60aeb27465fa40512c7d6573e3a1d9d36280f
Jacob Gunther <jacobgunther12@gmail.com>
2017-09-05 00:57:13 -0500

    Added some new bot listing sites

--- functions/update-sites.js
+++ functions/update-sites.js
@@ -3,20 +3,22 @@ const config = require("../config.json");
 const util = require("util");
 
 module.exports = (bot) => {
-    snekfetch.post("https://discordbots.org/api/bots/" + bot.user.id + "/stats").set("Authorization", config.api_keys.bot_list["discordbots.org"]).send({
-        server_count: bot.guilds.size,
-        shard_id: bot.shard.id,
-        shard_count: bot.shard.count
-    }).catch(() => {});
-    snekfetch.post("https://bots.discord.pw/api/bots/" + bot.user.id + "/stats").set("Authorization", config.api_keys.bot_list["bots.discord.pw"]).send({
-        server_count: bot.guilds.size,
-        shard_id: bot.shard.id,
-        shard_count: bot.shard.count
-    }).catch(() => {});
-    bot.shard.fetchClientValues("guilds.size").then((guilds) => {
+    bot.shard.fetchClientValues("guilds.size").then((server_count) => {
         snekfetch.post("https://www.carbonitex.net/discord/data/botdata.php").send({
             key: config.api_keys.bot_list.carbonitex,
-            servercount: guilds.reduce((a, b) => a + b, 0)
+            servercount: server_count.reduce((a, b) => a + b, 0)
+        }).catch(() => {});
+        snekfetch.post("https://list.passthemayo.space/api/bots/" + bot.user.id + "/").set("Authorization", config.api_keys.bot_list["list.passthemayo.space"]).send({
+            server_count
+        }).catch(() => {});
+        snekfetch.post("https://bots.iderp.space/api/bots/" + bot.user.id + "/").set("Authorization", config.api_keys.bot_list["bots.iderp.space"]).send({
+            server_count
+        }).catch(() => {});
+        snekfetch.post("https://discordbots.org/api/bots/" + bot.user.id + "/stats").set("Authorization", config.api_keys.bot_list["discordbots.org"]).send({
+            server_count
+        }).catch(() => {});
+        snekfetch.post("https://bots.discord.pw/api/bots/" + bot.user.id + "/stats").set("Authorization", config.api_keys.bot_list["bots.discord.pw"]).send({
+            server_count
         }).catch(() => {});
     }).catch(console.error);
 };
