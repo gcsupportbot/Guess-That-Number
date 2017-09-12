8391476582bfd08f50947e8ff8c4b904837c1d53
Jacob Gunther <jacobgunther12@gmail.com>
2017-09-12 15:23:55 -0500

    Disabled bot list logging until they fix their god damn site

--- functions/update-sites.js
+++ functions/update-sites.js
@@ -7,18 +7,18 @@ module.exports = (bot) => {
         snekfetch.post("https://www.carbonitex.net/discord/data/botdata.php").send({
             key: config.api_keys.bot_list.carbonitex,
             servercount: server_count.reduce((a, b) => a + b, 0)
-        }).catch(console.error);
+        }).catch(() => {});
         snekfetch.post("https://list.passthemayo.space/api/bots/" + bot.user.id + "/").set("Authorization", config.api_keys.bot_list["list.passthemayo.space"]).send({
             server_count: server_count.reduce((a, b) => a + b, 0)
-        }).catch(console.error);
+        }).catch(() => {});
         snekfetch.post("https://bots.iderp.space/api/bots/" + bot.user.id + "/").set("Authorization", config.api_keys.bot_list["bots.iderp.space"]).send({
             server_count: server_count.reduce((a, b) => a + b, 0)
-        }).catch(console.error);
+        }).catch(() => {});
         snekfetch.post("https://discordbots.org/api/bots/" + bot.user.id + "/stats").set("Authorization", config.api_keys.bot_list["discordbots.org"]).send({
             server_count: server_count.reduce((a, b) => a + b, 0)
-        }).catch(console.error);
+        }).catch(() => {});
         snekfetch.post("https://bots.discord.pw/api/bots/" + bot.user.id + "/stats").set("Authorization", config.api_keys.bot_list["bots.discord.pw"]).send({
             server_count: server_count.reduce((a, b) => a + b, 0)
-        }).catch(console.error);
+        }).catch(() => {});
     }).catch(console.error);
 };
