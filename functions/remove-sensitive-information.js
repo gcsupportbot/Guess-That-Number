a1ca342536cb61e9379134855975519916e37abe
Jacob Gunther <jacobgunther12@gmail.com>
2017-09-12 22:12:52 -0500

    Hide RethinkDB host from eval

--- functions/remove-sensitive-information.js
+++ functions/remove-sensitive-information.js
@@ -1,5 +1,12 @@
 const config = require("../config.json");
 
 module.exports = (data) => {
-    return data.split(config.token).join("-- BOT TOKEN --").split(config.api_keys.bot_list["discordbots.org"]).join("-- discordbots.org API KEY --").split(config.api_keys.bot_list["bots.discord.pw"]).join("-- bots.discord.pw API KEY").split(config.api_keys.datadog.api_key).join("-- DATADOG API KEY --").split(config.api_keys.datadog.app_key).join("-- DATADOG APP KEY --").split(config.api_keys.bot_list.carbonitex).join("-- CARBONITEX API KEY --");
+    return data
+        .split(config.token).join("-- BOT TOKEN --")
+        .split(config.api_keys.bot_list["discordbots.org"]).join("-- discordbots.org API KEY --")
+        .split(config.api_keys.bot_list["bots.discord.pw"]).join("-- bots.discord.pw API KEY --")
+        .split(config.rethink.host).join("-- RETHINKDB HOST IP ADDRESS --")
+        .split(config.api_keys.datadog.api_key).join("-- DATADOG API KEY --")
+        .split(config.api_keys.datadog.app_key).join("-- DATADOG APP KEY --")
+        .split(config.api_keys.bot_list.carbonitex).join("-- CARBONITEX API KEY --");
 };
