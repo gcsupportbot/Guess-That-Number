15ce75c4c6480ed0ab19d18f28d153b59e62a262
Jacob Gunther <jacobgunther12@gmail.com>
2017-09-23 01:59:52 -0500

    Removed unhandled rejection message

--- events/ready.js
+++ events/ready.js
@@ -18,7 +18,7 @@ module.exports = (bot, r) => {
 				if (error.code === 50001) return;
 				if (error.code === 50007) return;
 			}
-			console.error("Unhandled rejection caught.", error);
+			console.error(error);
 		});
 		process.on("uncaughtException", console.error);
 		r.table("toggle").run((error, response) => {
