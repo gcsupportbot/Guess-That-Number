abd1a14b454045c8772c0adf6e5aa56230725e42
PassTheMayo <PassTheMayo>
2017-10-30 16:04:48 -0500

    Fixed presence

--- src/functions/update-presence.js
+++ src/functions/update-presence.js
@@ -1,10 +1,8 @@
 const config = require("../config.json");
 
 module.exports = (bot) => {
-	bot.user.setPresence({
-		game: {
-			name: bot.guilds.size + " Servers | " + config.prefix + "help",
-			type: 0
-		}
+	bot.setStatus("online", {
+		name: bot.guilds.size + " Servers | " + config.prefix + "help",
+		type: 0
 	});
 };
