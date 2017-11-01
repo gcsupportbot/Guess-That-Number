5fe99669394c5d04f767cf0466f3c5dc3c984d03
PassTheMayo <16949253+PassTheMayo@users.noreply.github.com>
2017-11-01 10:39:46 -0500

    Added statistics websocket file

---
+++ src/website/static/js/statistics.js
@@ -0,0 +1,16 @@
+(function() {
+	var socket = io.connect("/guess-that-number", { secure: true });
+	var servers = document.getElementById("servers");
+	var users = document.getElementById("users");
+	var channels = document.getElementById("channels");
+	var commands = document.getElementById("commands");
+	var uptime = document.getElementById("uptime");
+	
+	socket.on("stats", function(data) {
+		servers.innerText = data.servers;
+		users.innerText = data.users;
+		channels.innerText = data.channels;
+		commands.innerText = data.commands;
+		uptime.innerText = data.uptime;
+	});
+})();
