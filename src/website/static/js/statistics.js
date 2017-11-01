19c5a10a401cc6c44c0f85347d8522e7f809ec5d
PassTheMayo <16949253+PassTheMayo@users.noreply.github.com>
2017-11-01 12:11:46 -0500

    Fixed socket.io path

--- src/website/static/js/statistics.js
+++ src/website/static/js/statistics.js
@@ -1,5 +1,5 @@
 (function() {
-	var socket = io.connect("/guess-that-number/statistics", { secure: true });
+	var socket = io.connect("/guess-that-number/statistics", { secure: true, path: "/guess-that-number/socket.io" });
 	var servers = document.getElementById("servers");
 	var users = document.getElementById("users");
 	var channels = document.getElementById("channels");
