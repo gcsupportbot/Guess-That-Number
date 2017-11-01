265dbc1db7b9bc6c9c2559d7704f5ecbea4ebeaf
PassTheMayo <16949253+PassTheMayo@users.noreply.github.com>
2017-11-01 14:39:43 -0500

    Move everything out of the try/catch

--- src/website/static/js/statistics.js
+++ src/website/static/js/statistics.js
@@ -1,24 +1,16 @@
 (function() {
-	try {
-		console.log = alert;
-		console.warn = alert;
-		console.error = alert;
-		
-		var socket = io.connect("/guess-that-number/statistics", { secure: true, path: "/guess-that-number/socket.io" });
-		var servers = document.getElementById("servers");
-		var users = document.getElementById("users");
-		var channels = document.getElementById("channels");
-		var commands = document.getElementById("commands");
-		var uptime = document.getElementById("uptime");
+	var socket = io.connect("/guess-that-number/statistics", { secure: true, path: "/guess-that-number/socket.io" });
+	var servers = document.getElementById("servers");
+	var users = document.getElementById("users");
+	var channels = document.getElementById("channels");
+	var commands = document.getElementById("commands");
+	var uptime = document.getElementById("uptime");
 
-		socket.on("stats", function(data) {
-			servers.innerText = data.servers;
-			users.innerText = data.users;
-			channels.innerText = data.channels;
-			commands.innerText = data.commands;
-			uptime.innerText = data.uptime;
-		});
-	} catch(e) {
-		alert(e);	
-	}
+	socket.on("stats", function(data) {
+		servers.innerText = data.servers;
+		users.innerText = data.users;
+		channels.innerText = data.channels;
+		commands.innerText = data.commands;
+		uptime.innerText = data.uptime;
+	});
 })();
