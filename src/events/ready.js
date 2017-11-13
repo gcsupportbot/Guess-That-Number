4d9d857005917deb3edf45d3f80bae9f4c5ffdc4
PassTheMayo <PassTheMayo>
2017-11-13 00:01:39 -0600

    Debugging

--- src/events/ready.js
+++ src/events/ready.js
@@ -9,9 +9,7 @@ module.exports = (bot, r) => {
 	bot.on("ready", () => {
 		log(bot.user.username + " is ready!");
 		bot.startuptime = Date.now();
-		process.on("unhandledRejection", (error) => {
-			console.log(require("util").inspect(error));
-		});
+		process.on("unhandledRejection", console.error);
 		process.on("uncaughtException", console.error);
 		r.table("toggle").run((error, response) => {
 			if (error) return handleDatabaseError(error);
