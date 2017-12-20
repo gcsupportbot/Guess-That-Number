3bab15d23a97f2a77dbc2991eaebea40a5098edd
PassTheMayo <jacobgunther12@gmail.com>
2017-12-19 22:15:10 -0600

    Fixed bug

--- src/functions/log-to-channel.js
+++ src/functions/log-to-channel.js
@@ -1,5 +1,5 @@
 const config = require("../config.json");
 
 module.exports = (bot, message) => {
-	bot.guilds.get(config.official_guild).channels.get(config.log_channel).send(message);
+	bot.guilds.get(config.official_guild).channels.get(config.log_channel).createMessage(message);
 };
