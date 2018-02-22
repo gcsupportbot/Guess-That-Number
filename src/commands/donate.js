949988915e260a6b89092d8d2f6a6f3f6ac9bd9f
Jacob Gunther <jacobgunther12@gmail.com>
2018-02-21 22:59:59 -0600

    Rename files, removed PayPal from donate command

--- src/commands/donate.js
+++ src/commands/donate.js
@@ -3,11 +3,10 @@ const config = require('../config.json');
 module.exports = {
 	commands: [
 		'donate',
-		'patreon',
-		'paypal'
+		'patreon'
 	],
 	usage: 'donate',
-	description: 'Get the donation links to support the creator.',
+	description: 'Get the donation links to support the creator and receive perks.',
 	category: 'General',
 	hidden: false,
 	execute: (bot, r, msg) => {
@@ -15,16 +14,11 @@ module.exports = {
 			embed: {
 				title: 'Donation Links',
 				color: 3066993,
-				description: 'Donating to PassTheMayo will help pay for the VPS that keeps the bots alive.',
+				description: 'Donating can help you recieve some insane perks in this bot. Check it out on our Patreon page.',
 				fields: [
 					{
 						name: 'Patreon',
 						value: config.links.patreon,
-						inline: true
-					},
-					{
-						name: 'PayPal',
-						value: config.links.paypal,
 						inline: true
 					}
 				]
