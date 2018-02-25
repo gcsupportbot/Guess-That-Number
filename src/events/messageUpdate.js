102a2e505f0f72fc7b70e90b92a28ce3fbc6356d
Jacob Gunther <jacobgunther12@gmail.com>
2018-02-25 15:02:43 -0600

    Fixed bug when updating message

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -1,5 +1,5 @@
 const handleMessage = require('../util/handleMessage.js');
 
 module.exports = (bot, r) => {
-	bot.on('messageUpdate', (msg) => handleMessage(bot, r, msg));
+	bot.on('messageUpdate', (msg, oldMsg) => oldMsg !== null && handleMessage(bot, r, msg));
 };
