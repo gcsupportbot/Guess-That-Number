15de52c1f5f01cf4fe76dbffb1f607bb67dee521
Jacob Gunther <jacobgunther12@gmail.com>
2018-03-04 16:54:28 -0600

    Changed presence message

--- src/util/updatePresence.js
+++ src/util/updatePresence.js
@@ -2,7 +2,7 @@ const config = require('../config.json');
 
 module.exports = (bot) => {
 	bot.editStatus('online', {
-		name: 'for ' + config.prefix + 'help',
+		name: bot.guilds.size + ' servers | ' + config.prefix + 'help',
 		type: 3
 	});
 };
