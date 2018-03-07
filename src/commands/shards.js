7300d6758d3f94ba712210bbc07a30cbb2949f6a
Jacob Gunther <jacobgunther12@gmail.com>
2018-03-06 19:51:22 -0600

    Changed shards command

--- src/commands/shards.js
+++ src/commands/shards.js
@@ -18,28 +18,28 @@ module.exports = {
 			...bot.shards.map((s) => [
 				s.id + (msg.channel.guild ? bot.guildShardMap[msg.channel.guild.id] === s.id ? ' (current)' : '' : ''),
 				s.status.toUpperCase(),
-				Object.keys(bot.guildShardMap).filter((id) => bot.guildShardMap[id] === s.id).length,
-				new Set([].concat.apply([], Object.keys(bot.guildShardMap).map((id) => bot.guilds.get(id).members.map((m) => m.id)))).size,
-				s.latency + 'ms'
+				Object.keys(bot.guildShardMap).filter((id) => bot.guildShardMap[id] === s.id).length.toLocaleString(),
+				new Set([].concat.apply([], Object.keys(bot.guildShardMap).map((id) => bot.guilds.get(id).members.map((m) => m.id)))).size.toLocaleString(),
+				s.latency.toLocaleString() + 'ms'
 			])
 		];
 		msg.channel.createMessage('```\n' + table(data, {
 			border: {
-				topBody: '',
-				topJoin: '',
-				topLeft: '',
-				topRight: '',
-				bottomBody: '',
-				bottomJoin: '',
-				bottomLeft: '',
-				bottomRight: '',
-				bodyLeft: '',
-				bodyRight: '',
-				bodyJoin: ' ',
-				joinBody: '-',
-				joinLeft: '',
-				joinRight: '',
-				joinJoin: ' '
+				topBody: '─',
+				topJoin: '┬',
+				topLeft: '┌',
+				topRight: '┐',
+				bottomBody: '─',
+				bottomJoin: '┴',
+				bottomLeft: '└',
+				bottomRight: '┘',
+				bodyLeft: '│',
+				bodyRight: '│',
+				bodyJoin: '│',
+				joinBody: '─',
+				joinLeft: '├',
+				joinRight: '┤',
+				joinJoin: '┼'
 			}
 		}) + '```');
 	}
