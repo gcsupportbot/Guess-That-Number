cbc04f7781d4bc8cbfafe346a88d533c12dfe35f
root <jacobgunther12@gmail.com>
2018-04-27 21:40:09 +0000

    Fixed

--- src/events/disconnect.js
+++
@@ -1,7 +0,0 @@
-const Logger = require('../Util/Logger.js');
-
-module.exports = (bot) => {
-	bot.on('disconnect', (error) => {
-		Logger.info('Client disconnected: ' + error + '. Reconnecting...');
-	});
-};
