3884e8e5768e427a08726f8da321d5f87914fe2e
Jacob Gunther <jacobgunther12@gmail.com>
2018-06-27 20:50:45 -0500

    Fixed bugs

--- src/Util/formatArbitrary.js
+++ src/Util/formatArbitrary.js
@@ -3,8 +3,6 @@ const config = require('../config.json');
 module.exports = (data) => {
 	data = data.replace(new RegExp(config.token, 'g'), '-- SENSITIVE INFORMATION --');
 	data = data.replace(new RegExp(config.rethinkdb.password, 'g'), '-- SENSITIVE INFORMATION --');
-	data = data.replace(new RegExp(config.webhooks.guilds.token, 'g'), '-- SENSITIVE INFORMATION --');
-	data = data.replace(new RegExp(config.webhooks.commands.token, 'g'), '-- SENSITIVE INFORMATION --');
 	for (const key in config.api_keys) {
 		data = data.replace(new RegExp(config.api_keys[key], 'g'), '-- SENSITIVE INFORMATION --');
 	}
